6071b37ea3a163cb9a3ed00661de447967407f76
onahonchloe-byte <onahonchloe@gmail.com>
2025-09-17 21:43:41 -0600

    added hero name and health

---
+++ hero.js
@@ -0,0 +1,27 @@
+class Hero{
+    #health
+
+    constructor(name, health, attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getHealth() {
+        return this.#health
+    }
+}
+const hero1 = new Hero("Thorin", 100,10);
+const hero2 = new Hero("Gandalf", 100, 8);
+
+console.log(hero1.getName());
+console.log(hero1.getHealth());
+console.log(hero2.getName());
+console.log(hero2.getHealth());
+
+hero1.showStats();
+hero2.showStats();
